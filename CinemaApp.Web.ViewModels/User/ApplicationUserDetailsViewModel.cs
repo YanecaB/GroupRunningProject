e897e914c5a0b0b2995f934d039195303d3a81da
YanecaB <yanislav12345678@gmail.com>
2024-12-08 20:45:48 +0200

    Started working on Account details

---
+++ CinemaApp.Web.ViewModels/User/ApplicationUserDetailsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace CinemaApp.Web.ViewModels.User
+{
+	public class ApplicationUserDetailsViewModel
+	{
+        public Guid Id { get; set; }
+
+        public string? Email { get; set; }
+
+        public bool IsBanned { get; set; }
+
+        public ICollection<string> Memberships { get; set; } = null!;
+
+        public ICollection<string> UserEvents { get; set; } = null!;
+
+        public ICollection<string> Notifications { get; set; } = null!;
+    }
+}
+
---
+++ CinemaApp.Web/Areas/Identity/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using CinemaApp.Data.Models;
+using CinemaApp.Web.ViewModels.User;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CinemaApp.Web.Areas.Identity.Controllers
+{
+    using static Common.ApplicationConstants;
+    
+    //[Authorize(Roles = UserRoleName)]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var user = await _userManager.FindByIdAsync(id.ToString());
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = new ApplicationUserDetailsViewModel
+            {
+                Id = user.Id,
+                Email = user.Email,
+                IsBanned = user.IsBanned,
+                Memberships = user.Memberships.Select(m => m.Group.Name).ToList(),
+                UserEvents = user.ApplicationUserEvents.Select(e => e.Event.Title).ToList(),
+                Notifications = user.Notifications.Select(n => n.Message).ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
+
